57497dbfa34e1ae62d07c5ed5512a3acac924c95
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-15 17:36:09 -0300

    Adicionando o loading spinner

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BiLoaderAlt } from 'react-icons/bi';
+
+import './Loading.css';
+
+export default function Loading() {
+  return <BiLoaderAlt className="loading" />;
+}
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import ProductCard from '../ProductCard/ProductCard';
 import fetchProducts from '../../api/fetchProducts';
+import Loading from '../Loading/Loading';
 
 import './Products.css';
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchProducts('iphone').then((response) => {
       setProducts(response);
-      console.log(response);
+      setIsLoading(false);
     });
   }, []);
 
   return (
-    <section className="products container">
-      {products.map((product) => (
-        <ProductCard key={product.id} data={product} />
-      ))}
-    </section>
+    (isLoading && <Loading />) || (
+      <section className="products container">
+        {products.map((product) => (
+          <ProductCard key={product.id} data={product} />
+        ))}
+      </section>
+    )
   );
 }
